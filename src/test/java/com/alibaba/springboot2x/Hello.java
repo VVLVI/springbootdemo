0875e09374b9dfaf61a06fec2957df9400acb97c
wly <13546299532@165.com>
2020-04-21 18:55:57 +0800

    新加了一个，hello类

---
+++ src/test/java/com/alibaba/springboot2x/Hello.java
@@ -0,0 +1,13 @@
+package com.alibaba.springboot2x;/*
+ *@Description
+ *@auther  lenovo
+ *@create 2020-04-21
+ */
+
+
+public class Hello {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+    }
+//    哼，我的妹妹一点不疼哥哥，不要不连。
+}
